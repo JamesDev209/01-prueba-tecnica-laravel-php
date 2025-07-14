21da9b53e2db53d38881fdda2cf5080591aac716
JamesDev209 <james1913_@hotmail.com>
2025-07-13 23:20:09 -0500

    se crearon las rutas del sistemas saas para empresas, planes, usuarios, dashboard y logout

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,33 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\CompanyUserController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\PlanController;
 
+use function Pest\Laravel\get;
+
+                                                //el nombre del metodo
+Route::get('/crear-admin', [AuthController::class, 'crearAdmin']);
 Route::get('/', [AuthController:: class, 'index'])-> name('login');
+
+Route::post('/salir', [AuthController::class, 'salir'])->name('salir');
+Route::post('/logear', [AuthController:: class, 'logear'])-> name('logear');
+
+
+Route::get('/home', [DashboardController:: class, 'index'])-> name('home');
+
+
+//Empresas
+Route::prefix('empresas')->group(function(){
+    Route::get('/' ,[CompanyController::class, 'index'])->name('empresas');
+});
+
+Route::prefix('planes')->group(function(){
+    Route::get('/' ,[PlanController::class, 'index'])->name('planes');
+});
+
+Route::prefix('usuarios')->group(function(){
+    Route::get('/' ,[CompanyUserController::class, 'index'])->name('usuarios');
+});
